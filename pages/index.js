151ea7f20ec7af4fdd5ddae18beaca6cf071cd67
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-13 12:56:12 +0900

    Fetch post data with graphQL

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head'
 
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwindcss', excerpt: 'Learn React with Tailwind' },
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8 '>
       <Head>
@@ -29,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts },
+  }
 }
---
+++ services/index.js
@@ -0,0 +1,36 @@
+import { request, gql } from 'graphql-request'
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            categories {
+              name
+              slug
+            }
+            featuredImage {
+              url
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.postsConnection.edges
+}
