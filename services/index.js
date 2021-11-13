316a46a6dcd871eb29c1f0c7df779c899d546a8d
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-13 22:00:15 +0900

    Add article details for post

--- services/index.js
+++ services/index.js
@@ -38,6 +38,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+        featuredImage {
+          url
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.post
+}
+
 export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails() {
@@ -56,7 +91,7 @@ export const getRecentPosts = async () => {
   return result.posts
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories } } }, last: 3) {
@@ -69,7 +104,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, { categories, slug })
 
   return result.posts
 }
