80d2f1227a340d0d0e6b00bf9662b7517faf7f84
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-16 13:22:27 +0900

    Add: Author Component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
-  return (
-    <div>
-      <h1>Author</h1>
-    </div>
-  )
+const Author = ({ author }) => {
+	return (
+		<div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+			<div className='absolute left-0 right-0 -top-14'>
+				<Image className='align-middle rounded-full' unoptimized src={author.photo.url} alt={author.name} height='100px' width='100px' />
+			</div>
+			<h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+			<p className='text-white text-lg'>{author.bio}</p>
+		</div>
+	)
 }
 
 export default Author
