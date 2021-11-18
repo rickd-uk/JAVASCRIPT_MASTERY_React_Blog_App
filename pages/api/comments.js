bd6e1e090c75cd545aebb9c7be1459c367162eb2
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-19 00:02:08 +0900

    Add: Comments component; now functional

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -32,3 +32,18 @@ export default async function comments(req, res) {
 		return res.status(500).send(error)
 	}
 }
+
+export const getComments = async (slug) => {
+	const query = gql`
+		query GetComments($slug: String!) {
+			comments(where: { post: { slug: $slug } }) {
+				name
+				createdAt
+				comment
+			}
+		}
+	`
+	const result = await request(graphqlAPI, query, { slug })
+
+	return result.comments
+}
